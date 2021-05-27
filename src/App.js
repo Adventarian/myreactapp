c601e46fdede65309b6f236d630e160d95c0246a
Adventarian <theadventarian@gmail.com>
2021-05-27 12:29:29 -0400

    abc's

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './images/bg-intro-desktop.png'
+import './images/bg-intro-mobile.png'
+import './images/icon.png'
+import './images/icon-error.svg'
+import './App.css';
+
+function App() {
+
+  return (
+    
+<main>
+    <div className="content">
+<h1>Learn to code by watching others</h1>
+      <p>See how experienced developers solve problems in real time.
+        Watching scripted tutorials is great, but understanding how
+        developers think is invaluable.</p>
+    </div>
+    <div className = "box">
+<button class = "first">
+  <strong>
+    <span class="bold">Try it free 7 days</span>
+  </strong>
+  <span class="nobold"> then $20/mo. thereafter</span>
+  </button> <br/>
+  <form>
+<input type="text" placeholder=""></input><br/>
+<input type="text" placeholder=""></input><br/>
+<input type="text" placeholder=""></input><br/>
+<input type="text" placeholder=""></input><br/> 
+<button class="second">
+  CLAIM YOUR FREE TRIAL
+  </button>
+  <h6>
+    By clicking the button, you are agreeing to our
+    <span class="pink">Terms and Services</span>
+        </h6>   
+  </form>
+    </div>
+    </main>
+  );
+}
+
+export default App;
